a2b17e0464528378c54b76c48892b21ac3043617
demo1\Mason Rumengan <masonrumengan@yahoo.com>
2021-03-04 14:36:57 +0700

    - use env DB_NAMES as array

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-const cron = require('node-cron')
-const moment = require('moment')
-const fs = require('fs')
-const spawn = require('child_process').spawn
+require('dotenv').config();
+const cron = require('node-cron');
+const moment = require('moment');
+const fs = require('fs');
+const spawn = require('child_process').spawn;
 
 // You can adjust the backup frequency as you like, this case will run once a day
-cron.schedule('* * * * *', () => {
-  // Use moment.js or any other way to dynamically generate file name
-  const fileName = `${process.env.DB_NAME}_${moment().format('YYYY_MM_DD')}.sql`;
-  const wstream = fs.createWriteStream(`/Path/You/Want/To/Save/${fileName}`);
-  console.log('---------------------');
-  console.log('Running Database Backup Cron Job');
-  const mysqldump = spawn('mysqldump', ['-u', process.env.DB_USER, `-p${process.env.DB_PASSWORD}`, process.env.DB_NAME])
+cron.schedule('0 0 * * *', () => {
+  const dbs = process.env.DB_NAMES.split(',');
+  for (i = 0; i < dbs.length; i++) {
+    let dbName = dbs[i].trim();
 
-  mysqldump
-    .stdout
-    .pipe(wstream)
-    .on('finish', () => {
-      console.log('DB Backup Completed!')
-    })
-    .on('error', (err) => {
-      console.log(err)
-    })
-})
+    const fileName = `${dbName}_${moment().format('YYYY_MM_DD_HH')}.sql`;
+    const wstream = fs.createWriteStream(`${process.env.BACKUP_DIR}/${fileName}`);
+    console.log('---------------------');
+    console.log('Running Database Backup Cron Job');
+    const mysqldump = spawn('mysqldump', ['-u', process.env.DB_USER, `-p${process.env.DB_PASSWORD}`, process.env.DB_NAME])
 
-console.log('Waiting', `${process.env.DB_NAME}_${moment().format('YYYY_MM_DD')}.sql`);
+    mysqldump
+      .stdout
+      .pipe(wstream)
+      .on('finish', () => {
+        console.log(new Date(), 'DB Backup ${dbName} Completed!')
+      })
+      .on('error', (err) => {
+        console.log(new Date(), err)
+      });
+  }
+}, {
+  timezone: "Asia/Jakarta"
+});
 
+console.log(new Date(), 'mysqlbackup running');
